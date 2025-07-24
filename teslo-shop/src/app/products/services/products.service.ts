29c773272560c886c050e60df31b8fb77722e5ef
Vladyslav Vagin <vladtwince@gmail.com>
2025-07-24 09:07:18 +0200

    up`date pipe image

---
+++ teslo-shop/src/app/products/services/products.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import type { ProductResponse } from '@products/interfaces/product.interface';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
+
+interface Options {
+  limit?: number;
+  offset?: number;
+  gender?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class ProductsService {
+  private http = inject(HttpClient);
+
+  getProducts(options: Options): Observable<ProductResponse> {
+    const { limit = 9, offset = 0, gender = '' } = options;
+    return this.http
+      .get<ProductResponse>(`${baseUrl}/products`, {
+        params: { limit, offset, gender }
+      })
+      .pipe(tap((response) => console.log(response)));
+  }
+}
