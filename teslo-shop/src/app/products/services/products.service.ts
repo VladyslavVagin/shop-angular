44a67ad27702de88e0713bc659b87e05360e7a14
Vladyslav Vagin <vladtwince@gmail.com>
2025-07-24 19:51:29 +0200

    auth

--- teslo-shop/src/app/products/services/products.service.ts
+++ teslo-shop/src/app/products/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import type { Product, ProductResponse } from '@products/interfaces/product.interface';
 import { environment } from 'src/environments/environment';
 
@@ -15,17 +15,34 @@ interface Options {
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
   private http = inject(HttpClient);
+  private productsCach = new Map<string, ProductResponse>();
+  private productCache = new Map<string, Product>();
 
   getProducts(options: Options): Observable<ProductResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
+
+    const key = `${limit}-${offset}-${gender}`;
+    if( this.productsCach.has(key)) {
+      return of(this.productsCach.get(key) as ProductResponse);
+    }
+
     return this.http
       .get<ProductResponse>(`${baseUrl}/products`, {
         params: { limit, offset, gender }
       })
-      .pipe(tap((response) => console.log(response)));
+      .pipe(
+        tap((response) => console.log(response)),
+        tap((response) => this.productsCach.set(key, response))
+      );
   }
 
   getProductByIdSlug(idSlug: string): Observable<Product> {
-     return this.http.get<Product>(`${baseUrl}/products/${idSlug}`);
+    if (this.productCache.has(idSlug)) {
+      return of(this.productCache.get(idSlug) as Product);
+    }
+
+     return this.http.get<Product>(`${baseUrl}/products/${idSlug}`).pipe(
+      tap((product) => this.productCache.set(idSlug, product))
+     );
   }
 }
