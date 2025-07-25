109a69e9e25b384daeb8ec5f5382ed2b345d06d2
Vladyslav Vagin <vladtwince@gmail.com>
2025-07-25 17:04:32 +0200

    errors

--- teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
+++ teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -4,10 +4,11 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import type { Product } from '@products/interfaces/product.interface';
 import { ProductCarouselComponent } from '@products/components/product-carousel/product-carousel.component';
 import { FormUtils } from '@utils/form-utils';
+import { FormErrorLabelComponent } from "@shared/components/form-error-label/form-error-label.component";
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarouselComponent, ReactiveFormsModule],
+  imports: [ProductCarouselComponent, ReactiveFormsModule, FormErrorLabelComponent],
   templateUrl: './product-details.component.html',
 })
 export class ProductDetailsComponent implements OnInit {
@@ -54,6 +55,7 @@ export class ProductDetailsComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.productForm.value);
+    const isValid = this.productForm.valid;
+    console.log(this.productForm.value, { isValid });
   }
 }
--- teslo-shop/src/app/shared/components/form-error-label/form-error-label.component.ts
+++ teslo-shop/src/app/shared/components/form-error-label/form-error-label.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { FormUtils } from '@utils/form-utils';
 
 @Component({
-  selector: 'app-form-error-label',
+  selector: 'form-error-label',
   imports: [],
-  templateUrl: './form-error-label.component.html'
+  templateUrl: './form-error-label.component.html',
 })
-export class FormErrorLabelComponent { }
+export class FormErrorLabelComponent {
+  control = input.required<AbstractControl>();
+
+  get errorMessage() {
+    const errors: ValidationErrors = this.control()?.errors || {};
+    return this.control()?.touched && Object.keys(errors).length > 0
+      ? FormUtils.getTextError(errors)
+      : null;
+  }
+}
