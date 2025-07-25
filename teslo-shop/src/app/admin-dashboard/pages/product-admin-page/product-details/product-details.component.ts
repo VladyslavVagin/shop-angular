ddfe6c749dd4171c956d6857604b2c51ae056340
Vladyslav Vagin <vladtwince@gmail.com>
2025-07-25 16:46:06 +0200

    update

--- teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
+++ teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -1,13 +1,59 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
 import type { Product } from '@products/interfaces/product.interface';
-import { ProductCarouselComponent } from "@products/components/product-carousel/product-carousel.component";
+import { ProductCarouselComponent } from '@products/components/product-carousel/product-carousel.component';
+import { FormUtils } from '@utils/form-utils';
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarouselComponent],
-  templateUrl: './product-details.component.html'
+  imports: [ProductCarouselComponent, ReactiveFormsModule],
+  templateUrl: './product-details.component.html',
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
   product = input.required<Product>();
+  fb = inject(FormBuilder);
   sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  productForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    slug: [
+      '',
+      [Validators.required, Validators.pattern(FormUtils.slugPattern)],
+    ],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    sizes: [[''], Validators.required],
+    images: [[]],
+    tags: [''],
+    gender: [
+      'men',
+      [Validators.required, Validators.pattern(/^(men|women|kid|unisex)$/)],
+    ],
+  });
+
+  ngOnInit(): void {
+    this.setFormValue(this.product());
+  }
+
+  onSizeClicked(size: string) {
+    const currentSizes = this.productForm.value.sizes || [];
+    if (currentSizes.includes(size)) {
+      currentSizes.splice(currentSizes.indexOf(size), 1);
+    } else {
+      currentSizes.push(size);
+    }
+
+    this.productForm.patchValue({ sizes: currentSizes });
+  }
+
+  setFormValue(formLike: Partial<Product>) {
+    this.productForm.patchValue(formLike as any);
+    this.productForm.patchValue({ tags: formLike.tags?.join(', ') });
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value);
+  }
 }
