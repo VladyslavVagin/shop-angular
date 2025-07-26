60c52d71591a78e0af7b8e93a583aeecde4b271d
Vladyslav Vagin <vladtwince@gmail.com>
2025-07-26 10:15:42 +0200

    update

--- teslo-shop/src/app/app.config.ts
+++ teslo-shop/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { loggingInterceptor } from '@shared/interceptors/logging.interceptor';
@@ -9,7 +9,7 @@ import { authInterceptor } from '@auth/interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withFetch(), withInterceptors([loggingInterceptor, authInterceptor])),
   ],
 };
